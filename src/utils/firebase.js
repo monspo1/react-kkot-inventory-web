5cbac55a4d8abaaa15bf1d4014df0575c65ac2f4
SUNG PIL MOON <monspo1@gmail.com>
2024-03-05 13:46:14 -0500

    fixed the firebase deploy issue - 2

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,23 +3,26 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
 //######################################################################
-//## Local credentials & for firebase DEPLOY (아래 상태 그대로 firebase deploy해)
+//## Local credentials & for firebase DEPLOY 
+//### ==> (이 부분은 npm run build할 때 필요한 거. 그 후 firebase deploy해)
 //######################################################################
-import  { myFirebaseConfig } from './../../credentials/credentials'
-const firebaseConfig = myFirebaseConfig;
+// import  { myFirebaseConfig } from './../../credentials/credentials'
+// const firebaseConfig = myFirebaseConfig;
 
 //######################################################################
-//## GitHub Actions workflow (firebase deploy할 때 이거 필요 없음. github에 push할 때만)
+//## GitHub Actions workflow 
+//### ==> 이 부분은 firebase deploy할 때 이거 필요 없음. github에 push하고 merge 때 필요!!!
 //######################################################################
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID
-// };
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
